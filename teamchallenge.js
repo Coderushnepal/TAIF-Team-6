51eace1f7f0b9eb43ad8a62ecd73bf53c6b9072e
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2019-09-27 10:24:59 +0545

    adding founder key value and checking if founder to assign CTO

--- teamchallenge.js
+++ teamchallenge.js
@@ -1,21 +1,23 @@
 let people = [
-    { name: 'Rabina', role: 'developer' },
-    { name: 'Sushmita', role: 'developer' },
-    { name: 'Nishla', role: 'designer' },
-    { name: 'Kanchan', role: 'Data analyst' }
+    { name: 'Rabina', role: 'developer', founder: true },
+    { name: 'Sushmita', role: 'developer', founder: false },
+    { name: 'Nishla', role: 'designer', founder: false },
+    { name: 'Kanchan', role: 'Data analyst', founder: false }
 ]
 
 // Displaying the names and roles of all the founding members
 for (i = 0; i < people.length; i++) {
     console.log(`${people[i].name} is a ${people[i].role}`)
+
     if (people[i].role === 'Data analyst') {
         console.log(`${people[i].name} is the one incharge of data`)
     }
+
+    // assigning CTO role to the member who is founder
+    if (people[i].founder) {
+        people[i].role = 'CTO'
+    }
 }
-
-
-// assigning CTO role to the first developer
-people[0].role = 'CTO'
 
 // adding new developer
 people.push({ name: 'Shristi', role: 'developer' })
